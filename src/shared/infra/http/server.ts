e16e9db40d2d13591d0df95c74144d81162b33c4
yuripalacio <yurifpalacio@gmail.com>
2021-12-22 19:26:33 -0300

    Remover CRON para teste de consumo RAM

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,7 +1,7 @@
-import { CronList } from "../../../crons";
+// import { CronList } from "../../../crons";
 
 import { app } from "./app";
 
 app.listen(3333, () => console.log("Server is running! port: 3333"));
 
-CronList();
+// CronList();
