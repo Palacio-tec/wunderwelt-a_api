5a2f8e3c60810c65ab8a1856595938d8357abe29
yuripalacio <yurifpalacio@gmail.com>
2022-01-06 19:56:15 -0300

    Adicionar novas variaveis ambiente

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,7 +1,7 @@
 export default {
   secret_token: process.env.APP_SECRET,
-  expires_in_token: "1d",
+  expires_in_token: process.env.TOKEN_EXPIRES_IN,
   secret_refresh_token: process.env.APP_SECRET_REFRESH,
-  expires_in_refresh_token: "30d",
+  expires_in_refresh_token: process.env.REFRESH_TOKEN_EXPIRES_IN,
   expires_refresh_token_days: 30,
 };
