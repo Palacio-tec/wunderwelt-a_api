77dbaf8a713de985ca6ffdf5eb14e9ad76af6ccc
yuripalacio <yurifpalacio@gmail.com>
2022-04-02 16:00:25 -0300

    Alterar descriçao do e-mail de RefundReminder
    Conforme solicitado, foi adicionado as informaçoes de data e horario de
    inicio da aula, assim como o tempo maximo para garantir os creditos de
    volta.
    Alem disse foi alterado o assunto e alguns textos do e-mail.

--- src/modules/events/useCases/sendCanRefoundReminderEvents/SendCanRefoundReminderEventsUseCase.ts
+++ src/modules/events/useCases/sendCanRefoundReminderEvents/SendCanRefoundReminderEventsUseCase.ts
@@ -6,7 +6,6 @@ import { resolve } from "path";
 import { IEventsRepository } from "@modules/events/repositories/IEventsRepository";
 import { ISchedulesRepository } from "@modules/schedules/repositories/ISchedulesRepository";
 import { IMailProvider } from "@shared/container/providers/MailProvider/IMailProvider";
-import { EventsRepository } from "@modules/events/infra/typeorm/repositories/EventsRepository";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { IParametersRepository } from "@modules/parameters/repositories/IParametersRepository";
 
@@ -31,13 +30,14 @@ class SendCanRefoundReminderEventsUseCase {
 
   async execute(date: Date): Promise<void> {
     const refundPeriodReminder = await this.parametersRepository.findByReference('RefundPeriodReminder');
+    const refundTimeLimit = await this.parametersRepository.findByReference('RefundTimeLimit');
 
     const refundPeriodReminderValue = Number(refundPeriodReminder.value);
 
     const startDate = this.dateProvider.addDaysInDate(date, refundPeriodReminderValue);
     const startDateFormated = this.dateProvider.parseFormatUTC(startDate);
 
-    const endDate = this.dateProvider.addHoursInDate(startDate, 1);
+    const endDate = this.dateProvider.addHoursInDate(startDate, 24 * 120);
     const endDateFormated = this.dateProvider.parseFormatUTC(endDate);
 
     // console.log( `[EventsCanRefound - ${date}] startDate = '${startDateFormated}' - endDate ='${endDateFormated}'` )
@@ -63,15 +63,18 @@ class SendCanRefoundReminderEventsUseCase {
 
       schedules.map(async (schedule) => {
         const { user } = schedule;
+        const eventDate = this.dateProvider.parseFormat(event.start_date, "DD-MM-YYYY [às] HH:mm")
 
         const variables = {
           name: user.name,
           title,
+          eventDate,
+          refundTimeLimit: refundTimeLimit.value,
         };
   
         this.mailProvider.sendMail(
           user.email,
-          "Deseja cancelar a inscrição na aula?",
+          "Deseja manter ou cancelar sua próxima aula?",
           variables,
           templatePath
         );
