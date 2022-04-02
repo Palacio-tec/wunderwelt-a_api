8747b7a9bc2ff373a73549bc27b7dee09d97632a
yuripalacio <yurifpalacio@gmail.com>
2022-04-02 16:02:50 -0300

    Ajustar range de envio

--- src/modules/events/useCases/sendCanRefoundReminderEvents/SendCanRefoundReminderEventsUseCase.ts
+++ src/modules/events/useCases/sendCanRefoundReminderEvents/SendCanRefoundReminderEventsUseCase.ts
@@ -37,7 +37,7 @@ class SendCanRefoundReminderEventsUseCase {
     const startDate = this.dateProvider.addDaysInDate(date, refundPeriodReminderValue);
     const startDateFormated = this.dateProvider.parseFormatUTC(startDate);
 
-    const endDate = this.dateProvider.addHoursInDate(startDate, 24 * 120);
+    const endDate = this.dateProvider.addHoursInDate(startDate, 1);
     const endDateFormated = this.dateProvider.parseFormatUTC(endDate);
 
     // console.log( `[EventsCanRefound - ${date}] startDate = '${startDateFormated}' - endDate ='${endDateFormated}'` )
