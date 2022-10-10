89040c74690f7bff7f05a47e00da13ecc84ecc73
Yuri Palacio <yurifpalacio@gmail.com>
2022-10-10 19:14:05 -0300

    Remover validação de ambiente

--- src/shared/infra/http/manager-cron/index.ts
+++ src/shared/infra/http/manager-cron/index.ts
@@ -9,24 +9,20 @@ import PreviewEventsWillStart from "./jobs/PreviewEventsWillStart";
 import CreditsWillExpired from "./jobs/CreditsWillExpired";
 import UpdateUserCredits from "./jobs/UpdateUserCredits"
 
-const { ENVIRONMENT } = process.env
-
 class ManagerCron{
   private jobs: ScheduledTask[];
 
   constructor() {
-    this.jobs = (ENVIRONMENT === 'production')
-      ? [
-        EventsReminder,
-        EventsWillStart,
-        EventsWithoutStudent,
-        EventsCanRefound,
-        EventsNewsletter,
-        PreviewEventsWillStart,
-        CreditsWillExpired,
-        UpdateUserCredits,
-      ]
-      : [];
+    this.jobs = [
+      EventsReminder,
+      EventsWillStart,
+      EventsWithoutStudent,
+      EventsCanRefound,
+      EventsNewsletter,
+      PreviewEventsWillStart,
+      CreditsWillExpired,
+      UpdateUserCredits,
+    ];
   };
 
   run() {
